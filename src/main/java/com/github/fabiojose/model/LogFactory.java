e0986235ff23d544800da4a16c7a7b740f245939
Fabio José <fabiojose@gmail.com>
2019-07-12 16:16:40 -0300

    Log entry parser complete
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- src/main/java/com/github/fabiojose/model/LogFactory.java
+++ src/main/java/com/github/fabiojose/model/LogFactory.java
@@ -2,9 +2,19 @@ package com.github.fabiojose.model;
 
 import static java.util.Objects.requireNonNull;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.ParseException;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 
@@ -12,9 +22,11 @@ import java.util.stream.Stream;
  *
  */
 public class LogFactory {
-/*
-23:49:08.277      038 – F.MASSA                           1 1:02.852           44,275
- */
+	
+	private static final Logger log = 
+			LoggerFactory.getLogger(LogFactory.class);
+	
+	private static final Locale PT_BR = new Locale("pt", "BR");
 	
 	private static final int HORA_START_INDEX = 0;
 	private static final int HORA_END_INDEX = 11;
@@ -28,10 +40,13 @@ public class LogFactory {
 	private static final int TEMPO_VOLTA_START_INDEX = 61;
 	private static final int TEMPO_VOLTA_END_INDEX = 76;
 	
-	private static final int VELOCIDADE_VOLTA_START_INDEX = 80;
-	private static final int VELOCIDADE_VOLTA_END_INDEX = 86;
+	private static final int VELOCIDADE_MEDIA_VOLTA_START_INDEX = 80;
+	private static final int VELOCIDADE_MEDIA_VOLTA_END_INDEX = 86;
 	
 	static String extractField(String entry, int startIndex, int endIndex) {
+		
+		log.debug("Extract: startIndex={}, endIndex={}, >{}<",
+				startIndex, endIndex, entry);
 		
 		return IntStream.range(startIndex, entry.length())
 				.filter(i -> i <= endIndex)
@@ -41,18 +56,81 @@ public class LogFactory {
 				.trim();
 	}
 	
+	static Duration toDuration(String time) {
+		
+		LocalTime midnight = LocalTime.parse("00:00:00");
+		
+		LocalTime durationAsLocalTime =
+		LocalTime.parse("00:" + time, 
+				DateTimeFormatter.ofPattern("[H:]m:ss[.SSS]"));
+		
+		return Duration.between(midnight, durationAsLocalTime);
+	}
+	
+	static Float parsePtBRFloat(String value) throws ParseException {
+		
+		DecimalFormatSymbols brasilFloat = new DecimalFormatSymbols(PT_BR);
+		brasilFloat.setDecimalSeparator(',');
+		
+		DecimalFormat decimalFormat = new DecimalFormat("0.#", brasilFloat);
+		
+		return decimalFormat.parse(value).floatValue();
+	}
+	
 	public static Log fromLogEntry(String logEntry) {
 		requireNonNull(logEntry);
 		
 		String entry = 
 			Stream.of(logEntry)
+				.map(String::trim)
 				.filter(e -> !e.isEmpty())
 				.findFirst()
 				.orElseThrow(() -> new IllegalArgumentException());
 	
-		String horaString = extractField(entry, HORA_START_INDEX, HORA_END_INDEX);
-		System.out.println(horaString);
+		String horaString = extractField(entry,
+				HORA_START_INDEX, HORA_END_INDEX);
+		log.debug("Hora: >{}<", horaString);
 		
-		return null;
+		LocalTime hora = LocalTime.parse(horaString);
+		log.debug("[PARSED] Hora: {}", hora);
+		
+		String pilotoString = extractField(entry,
+				PILOTO_START_INDEX, PILOTO_END_INDEX);
+		log.debug("Piloto: >{}<", pilotoString);
+		
+		Piloto piloto = Piloto.fromString(pilotoString);
+		log.debug("[PARSED] Piloto: {}", piloto);
+		
+		String voltaString = extractField(entry,
+				VOLTA_START_INDEX, VOLTA_END_INDEX);
+		log.debug("Volta: >{}<", voltaString);
+		
+		Short volta = Short.parseShort(voltaString);
+		log.debug("[PARSED] Volta: {}", volta);
+		
+		String tempoVoltaString = extractField(entry,
+				TEMPO_VOLTA_START_INDEX, TEMPO_VOLTA_END_INDEX);
+		log.debug("Tempo Volta: >{}<", tempoVoltaString);
+
+		Duration tempoVolta = toDuration(tempoVoltaString);
+		log.debug("[PARSED] Tempo Volta: {}", tempoVolta);
+		
+		String velocidadeMediaVoltaString = extractField(entry,
+				VELOCIDADE_MEDIA_VOLTA_START_INDEX,
+				VELOCIDADE_MEDIA_VOLTA_END_INDEX);
+		log.debug("Velocidade Média Volta: >{}<", velocidadeMediaVoltaString);
+		
+		float velocidadeMediaVolta = 0;
+		try {
+			velocidadeMediaVolta = 
+					parsePtBRFloat(velocidadeMediaVoltaString);
+			log.debug("[PARSED] Velocidade Média Volta: {}",
+					velocidadeMediaVolta);
+		}catch(ParseException e) {
+			log.error(e.getMessage(), e);
+			throw new IllegalArgumentException(e.getMessage(), e);
+		}
+		
+		return new Log(hora, piloto, volta, tempoVolta, velocidadeMediaVolta);
 	}
 }
--- src/test/java/com/github/fabiojose/model/LogFactoryTests.java
+++ src/test/java/com/github/fabiojose/model/LogFactoryTests.java
@@ -3,6 +3,11 @@ package com.github.fabiojose.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.text.ParseException;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -37,6 +42,37 @@ public class LogFactoryTests {
 	}
 	
 	@Test
+	public void duration_ok() {
+		String time = "1:00.000";
+		Duration expected = Duration.ofMinutes(1);
+		
+		// act
+		Duration actual = LogFactory.toDuration(time);
+		
+		// assert
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void float_ptbr_err() {
+		String floatValue = "K3K0";
+		
+		assertThrows(ParseException.class, () -> {
+			LogFactory.parsePtBRFloat(floatValue);
+		});
+	}
+	
+	@Test
+	public void float_ptbr_ok() throws ParseException {
+		float expected = 3.345f;
+		String floatValue = "3,345";
+		
+		float actual = LogFactory.parsePtBRFloat(floatValue);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
 	public void hora_entry_ok() {
 		// setup
 		String expected = "23:49:08.277";
@@ -50,6 +86,31 @@ public class LogFactoryTests {
 			
 		// assert
 		assertEquals(expected, actual);
+	} 
+	
+	@Test
+	public void hora_entry_parse_err() {
+		// setup
+		String logEntry = "23K49K08@277      038 – F.MASSA                           1		1:02.852           44,275";
+		
+		// assert
+		assertThrows(DateTimeParseException.class, () -> {
+			// act
+			LogFactory.fromLogEntry(logEntry);
+		});
+	}
+	
+	@Test
+	public void hora_entry_parse_ok() {
+		// setup
+		LocalTime expected = LocalTime.parse("23:49:08.277");
+		String logEntry = "23:49:08.277      038 – F.MASSA                           1		1:02.852           44,275";
+		
+		// act
+		Log actual = LogFactory.fromLogEntry(logEntry);
+					
+		// assert
+		assertEquals(expected, actual.getHora());
 	}
 	
 	@Test
@@ -69,6 +130,20 @@ public class LogFactoryTests {
 	}
 	
 	@Test
+	public void piloto_entry_parse_ok() {
+		// setup
+		Piloto expected = new Piloto("038", "F.MASSA");
+		String logEntry = "23:49:08.277      038 – F.MASSA                           1		1:02.852           44,275";
+		
+		// act
+		Log actual = LogFactory.fromLogEntry(logEntry);
+		
+		// assert
+		assertEquals(expected.getCodigo(), actual.getPiloto().getCodigo());
+		assertEquals(expected.getNome(), actual.getPiloto().getNome());
+	}
+	
+	@Test
 	public void volta_entry_ok() {
 		// setup
 		String expected = "1";
@@ -82,6 +157,33 @@ public class LogFactoryTests {
 			
 		// assert
 		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void volta_entry_parse_err() {
+		// setup
+		String logEntry = "23:49:08.277      038 – F.MASSA                           $		1:02.852           44,275";
+		
+		// assert
+		assertThrows(NumberFormatException.class, () -> {
+			// act
+			LogFactory.fromLogEntry(logEntry);
+		});
+		
+	}
+	
+	@Test
+	public void volta_entry_parse_ok() {
+		// setup
+		short expected = Short.valueOf("1");
+		String logEntry = "23:49:08.277      038 – F.MASSA                           1		1:02.852           44,275";
+		
+		// act
+		Log actual = LogFactory.fromLogEntry(logEntry);
+					
+		// assert
+		assertEquals(expected, actual.getVolta());
+		
 	}
 	
 	@Test
@@ -101,6 +203,30 @@ public class LogFactoryTests {
 	}
 	
 	@Test
+	public void tempo_volta_entry_parse_err() {
+		// setup
+		String logEntry = "23:49:08.277      038 – F.MASSA                           1		1K02@852           44,275";
+		
+		// assert
+		assertThrows(DateTimeParseException.class, () -> {
+			LogFactory.fromLogEntry(logEntry);
+		});
+	}
+	
+	@Test
+	public void tempo_volta_entry_parse_ok() {
+		// setup
+		Duration expected = Duration.parse("PT1M2.852S");
+		String logEntry = "23:49:08.277      038 – F.MASSA                           1		1:02.852           44,275";
+		
+		// act
+		Log actual = LogFactory.fromLogEntry(logEntry);
+		
+		assertEquals(expected, actual.getTempoVolta());
+		
+	}
+	
+	@Test
 	public void velocidade_volta_entry_ok() {
 		// setup
 		String expected = "44,275";
@@ -116,4 +242,24 @@ public class LogFactoryTests {
 		assertEquals(expected, actual);
 	}
 	
+	@Test
+	public void velocidade_volta_entry_parse_ok() {
+		// setup
+		float expected = 44.275f;
+		String logEntry = "23:49:08.277      038 – F.MASSA                           1		1:02.852           44,275";
+		
+		Log actual = LogFactory.fromLogEntry(logEntry);
+		
+		assertEquals(expected, actual.getVelocidadeMediaVolta());
+	}
+	
+	@Test
+	public void velocidade_volta_entry_parse_err() {
+		String logEntry = "23:49:08.277      038 – F.MASSA                           1		1:02.852           Y4,275";
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			LogFactory.fromLogEntry(logEntry);
+		});
+		
+	}
 }
