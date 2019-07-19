1fc9393b0abd6fa823a43117f7112366ddfa559e
Fabio José <fabiojose@gmail.com>
2019-07-19 15:12:58 -0300

    Implementar hashCode e equals
    Signed-off-by: Fabio José <fabiojose@gmail.com>

--- src/main/java/com/github/fabiojose/model/Piloto.java
+++ src/main/java/com/github/fabiojose/model/Piloto.java
@@ -38,6 +38,31 @@ public class Piloto {
 		return "Piloto [codigo=" + codigo + ", nome=" + nome + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Piloto other = (Piloto) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
 	/**
 	 * Parses strings with this format: {@code 038 – F.MASSA}
 	 * @param string 
